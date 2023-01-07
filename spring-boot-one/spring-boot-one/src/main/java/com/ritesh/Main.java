66201b4b498933054a0cabdcb5b2eeac204ec53e
RiteshPuvvada <riteshpuvvada@gmail.com>
2023-01-07 17:15:45 +0530

    mvc

--- spring-boot-one/spring-boot-one/src/main/java/com/ritesh/Main.java
+++ spring-boot-one/spring-boot-one/src/main/java/com/ritesh/Main.java
@@ -2,11 +2,20 @@ package com.ritesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
         //System.out.println("Hello World");
+    }
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        return new GreetResponse("Hello");
+    }
+    record GreetResponse(String greet){
     }
 }
