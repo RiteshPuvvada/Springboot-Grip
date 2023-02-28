4288f24e20e802406d365e1e27885de5695fcc6c
RiteshPuvvada <riteshpuvvada@gmail.com>
2023-02-28 23:11:28 +0530

    model

---
+++ spring-boot-two/src/main/java/com/ritesh/springboottwo/SpringBootTwoApplication.java
@@ -0,0 +1,13 @@
+package com.ritesh.springboottwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwoApplication.class, args);
+	}
+
+}
