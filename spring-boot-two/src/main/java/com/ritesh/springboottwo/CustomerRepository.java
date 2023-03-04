0879e675ff37db3ddfd723f7466edf55717c87c9
RiteshPuvvada <riteshpuvvada@gmail.com>
2023-03-04 19:45:44 +0530

    read customer api

--- spring-boot-two/src/main/java/com/ritesh/springboottwo/CustomerRepository.java
+++ spring-boot-two/src/main/java/com/ritesh/springboottwo/CustomerRepository.java
@@ -3,5 +3,5 @@ package com.ritesh.springboottwo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    
+
 }
--- spring-boot-two/src/main/java/com/ritesh/springboottwo/SpringBootTwoApplication.java
+++ spring-boot-two/src/main/java/com/ritesh/springboottwo/SpringBootTwoApplication.java
@@ -2,14 +2,26 @@ package com.ritesh.springboottwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customer")
 public class SpringBootTwoApplication {
+	private final CustomerRepository customerRepository;
+	public SpringBootTwoApplication(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTwoApplication.class, args);
 	}
-
+	@GetMapping
+	public List<Customer> getCustomer(){
+		return customerRepository.findAll();
+	}
 }
