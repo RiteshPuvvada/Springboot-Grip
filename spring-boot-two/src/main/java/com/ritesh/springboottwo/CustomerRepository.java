61620774ca5f097dc4bb558fc3d040103ff0a033
RiteshPuvvada <riteshpuvvada@gmail.com>
2023-03-04 19:23:29 +0530

    added customer repo

---
+++ spring-boot-two/src/main/java/com/ritesh/springboottwo/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.ritesh.springboottwo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    
+}
