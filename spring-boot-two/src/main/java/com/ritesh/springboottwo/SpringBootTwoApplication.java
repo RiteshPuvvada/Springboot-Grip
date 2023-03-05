9e8c27c7e8987b8b7647a952794580fbeb2948a4
RiteshPuvvada <riteshpuvvada@gmail.com>
2023-03-05 20:01:33 +0530

    post mapping api

--- spring-boot-two/src/main/java/com/ritesh/springboottwo/SpringBootTwoApplication.java
+++ spring-boot-two/src/main/java/com/ritesh/springboottwo/SpringBootTwoApplication.java
@@ -2,9 +2,7 @@ package com.ritesh.springboottwo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,19 @@ public class SpringBootTwoApplication {
 	@GetMapping
 	public List<Customer> getCustomer(){
 		return customerRepository.findAll();
+	}
+
+	static record NewCustomerRequest(
+		String name,
+		String email,
+		Integer age
+	){}
+	@PostMapping
+	public void addCustomer(@RequestBody NewCustomerRequest request){
+		Customer customer =  new Customer();
+		customer.setName(request.name());
+		customer.setEmail(request.email());
+		customer.setAge(request.age());
+		customerRepository.save(customer);
 	}
 }
